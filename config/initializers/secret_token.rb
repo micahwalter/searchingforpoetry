45c6c7dbcf9f0a3f3a7a9a64e3ff6c073d03a5fe
Micah Walter <micah@micahwalter.com>
2011-03-29 18:56:06 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Searchingforpoetry::Application.config.secret_token = 'd9ad146e73ef6635a5f8d9e59bff85ca184016b004589c0e5ef743d61b91bb0670b18a4fc35689f507d752c67de537b4514988b26df475f5a1423f6fe72a1d49'
