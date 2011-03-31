9d2d7f6d7f136cefd39c00d6911d85111eeeeabe
Micah Walter <micah@micahwalter.com>
2011-03-30 23:00:35 -0400

    updated title var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
     
   def home
+    @title = "Searching For Poetry"
   end
   
 end
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,9 +2,11 @@ class PoemsController < ApplicationController
   layout "poems"
   
   def shebeingbrand
+    @title = "she being Brand"
   end
   
   def theredwheelbarrow
+    @title = "The Red Wheelbarrow"
   end
   
 end
